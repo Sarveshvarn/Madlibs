2fa71f8dbf5c00bb9eaeab47e613c8cf933801ff
Sarveshvarn Ramprasad <71520735+Sarveshvarn@users.noreply.github.com>
2022-03-02 18:55:33 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,207 @@
+# Creating a MadLib Game
+import time
+
+# Introduction to the game
+print("Welcome to Mad-Libs")
+time.sleep(3)
+print('')
+print("This is the most popular game in the world")
+time.sleep(3)
+print('')
+print("Show don't tell right!")
+time.sleep(2)
+print('')
+print("let's go")
+time.sleep(3)
+print('')
+
+# Your first Mad-Lib
+print("Mad-Lib Number 1")
+time.sleep(3)
+
+# Prompting the user to type their name
+Name = input("What is your name?: ")
+time.sleep(1)
+print('')
+# Prompting the user to type any adjective
+Adj1 = input("Enter an Adjective: ")
+time.sleep(1)
+print('')
+# Prompting the user to type any Plural Noun
+Noun1 = input("Enter a plural noun: ")
+time.sleep(1)
+print('')
+# Prompting the user to type any noun
+Noun2 = input("Enter any noun: ")
+time.sleep(3)
+print('')
+
+print("Your Rhyme is!")
+time.sleep(5)
+print('')
+
+# Printing the Rhyme 
+print("Roses are" ,Adj1)
+print(Noun1, "are blue")
+print("I cannot start my day without",Noun2)
+
+time.sleep(9)
+
+print('')
+print("Interested in doing another one")
+time.sleep(5)
+print("Here you go")
+time.sleep(5)
+print('')
+
+# Your Second Mad-Lib 
+print("Mad-Lib Number 2")
+time.sleep(3)
+
+# Prompting the user to type any noun
+Noun3 = input("Enter any noun: ")
+time.sleep(1)
+print('')
+# Prompting the user to type any adjective of quality
+Adj2 = input("Enter any adjective of quality: ")
+time.sleep(1)
+print('')
+# Prompting the user to type any name of a bird
+Noun4 = input("Enter any bird: ")
+time.sleep(3)
+print('')
+
+# Printing the Rhyme 
+print("Your Rhyme is!")
+time.sleep(5)
+print('')
+
+print("After hiding the painting in his",Noun3)
+print("for two years, he grew",Adj2)
+print("and tried to sell it to a/an",Noun4)
+print("in Florence, but was caught.")
+
+time.sleep(9)
+
+print('')
+print("Here is a Mad-Lib in the form of story")
+time.sleep(3)
+print('')
+
+# Your third Mad-Lib
+print("Mad-Lib Number 3")
+time.sleep(3)
+
+print("Source for this Mad-Lib")
+time.sleep(2)
+print("https://www.thespruce.com/baby-shower-mad-lib-game-1196934")
+time.sleep(10)
+print('')
+
+Adj3 = input("Enter any adjective: ")
+time.sleep(1)
+print('')
+
+Noun5 = input("Enter any noun: ")
+time.sleep(1)
+print('')
+
+Noun6 = input("Enter any plural noun: ")
+time.sleep(1)
+print('')
+
+Per = input("Enter any female name: ")
+time.sleep(1)
+print('')
+
+Adj4 = input("Enter any adjective: ")
+time.sleep(1)
+print('')
+
+Art = input("Enter any article of clothing: ")
+time.sleep(1)
+print('')
+
+Noun7 = input("Enter any noun: ")
+time.sleep(1)
+print('')
+
+City = input("Enter any name of a city: ")
+time.sleep(1)
+print('')
+
+Noun8 = input("Enter any plural noun: ")
+time.sleep(1)
+print('')
+
+Adj5 = input("Enter any adjective: ")
+time.sleep(1)
+print('')
+
+Parbody1 = input("Enter any part of the body: ")
+time.sleep(1)
+print('')
+
+Alpha = input("Enter any Letter of the Alphabet: ")
+time.sleep(1)
+print('')
+
+Celebrity = input("Enter any name of a celebrity: ")
+time.sleep(1)
+print('')
+
+Noun9 = input("Enter any plural noun: ")
+time.sleep(1)
+print('')
+
+Adj6 = input("Enter any adjective: ")
+time.sleep(1)
+print('')
+
+Place = input("Enter any place: ")
+time.sleep(1)
+print('')
+
+Parbody2 = input("Enter any part of the body: ")
+time.sleep(3)
+print('')
+
+print("Your story is!")
+time.sleep(5)
+
+# Printing the story
+print("There are many",Adj3)
+print("ways to choose a/an",Noun5)
+print("to read. First, you could ask for recommendations from your friends and",Noun6)
+print("Just don't ask Aunt",Per)
+print("--she only reads",Adj4)
+print("books with",Art)
+print("-ripping goddesses on the cover. If your friends and family are of no help, try checking out the",Noun7)
+print("Review in the",City)
+print("Times. If the",Noun8)
+print("featured there are too",Adj5)
+print("for your taste, try something alittle more low-",Parbody1)
+print(", like ",Alpha)
+print(": the ",Celebrity)
+print("Magazine, or ",Noun9)
+print("Magazine. You could also choose a book the ",Adj6)
+print("-fashioned way. Head to the your local library or ",Place)
+print("and browse the shelves until something catches your",Parbody2)
+
+time.sleep(9)
+print('')
+
+# Conclusion to the game
+print("And there you have it")
+time.sleep(1)
+print('')
+
+print("A beautiful and exciting game of Mad-libs")
+time.sleep(1)
+print('')
+
+print("Hope you enjoyed playing this game!")
+time.sleep(1)
+print('')
+
+print("Thank you")
